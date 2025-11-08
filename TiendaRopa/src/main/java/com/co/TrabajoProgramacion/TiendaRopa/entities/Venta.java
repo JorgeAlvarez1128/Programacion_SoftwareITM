78bc43bb990caa06fa1128a4ab34a89ecfc65a2c
Jeronimo <castrillonj041@gmail.com>
2025-11-07 22:03:25 -0500

    creacion de todo lo q faltaba

---
+++ TiendaRopa/src/main/java/com/co/TrabajoProgramacion/TiendaRopa/entities/Venta.java
@@ -0,0 +1,34 @@
+package com.co.TrabajoProgramacion.TiendaRopa.entities;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "Ventas")
+public class Venta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "VentaId")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ClienteId", nullable = false) // FK a Clientes.ClienteId
+    private Cliente cliente;
+
+    @Column(name = "FechaVenta", nullable = false)
+    private LocalDateTime fechaVenta;
+
+    @Column(name = "Total", nullable = false)
+    private Double total;
+
+    @OneToMany(mappedBy = "venta", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<VentaItem> items = new ArrayList<>();
+
+
+}
